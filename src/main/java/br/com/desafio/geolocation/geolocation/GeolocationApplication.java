e805a5c526d7475b194ca81a1b23164b03983174
carlosjrs006 <carlosjrs06.cj@gmail.com>
2024-04-22 14:33:07 -0300

    ajuste na horas que esta salvando

--- src/main/java/br/com/desafio/geolocation/geolocation/GeolocationApplication.java
+++ src/main/java/br/com/desafio/geolocation/geolocation/GeolocationApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.util.TimeZone;
+
 @EnableFeignClients
 @SpringBootApplication
 @ComponentScan("br.com.desafio.geolocation")
 public class GeolocationApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
 		SpringApplication.run(GeolocationApplication.class, args);
 	}
 
